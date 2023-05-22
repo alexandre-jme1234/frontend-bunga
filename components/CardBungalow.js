32dc1cdd82cc4b7c6c6e17380a0d9aa44abd5cb0
bade <infoabde@gmail.com>
2023-05-22 17:00:49 +0200

    navigate vers hhhome

--- components/CardBungalow.js
+++ components/CardBungalow.js
@@ -38,7 +38,7 @@ export default function CardBungalow(props) {
           <AspectRatio w="100%" ratio={16 / 9}>
             <Image
               source={{
-                uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
+                uri: props.image,
               }}
               alt="image"
             />
