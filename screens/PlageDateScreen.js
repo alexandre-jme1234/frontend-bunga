1ed43d1948d6fa626087ea6eb1113d9d7111da76
jammealexandre28 <jammealexandre1@gmail.com>
2023-05-22 17:24:39 +0200

    DataPlage en cours

--- screens/PlageDateScreen.js
+++ screens/PlageDateScreen.js
@@ -1,10 +1,27 @@
 
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { VStack, Text } from "native-base";
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function PlageDateScreen({ navigation }) {
  return (
    <View style={styles.background}>
-     <Text>Home Screen</Text>
+    <VStack
+        space={1}
+        alignItems="flex-start"
+        paddingLeft="10"
+        marginBottom="5"
+        marginTop="5"
+      >
+        <Text bold fontSize="sm">
+        Contacter le bailleur
+        </Text>
+        <Text fontSize="xs">Pour entamer les négociations avec le propriétaire de bungalows.
+        Sélectionner les lots de </Text>
+      </VStack>
+      <VStack style={styles.containerPlageData}>
+
+
+      </VStack>
      <Button
        title="Go to Congratulation"
        onPress={() => navigation.navigate('Congratulation')}
@@ -23,4 +40,10 @@ const styles = StyleSheet.create({
       width: '100%',
       height: '100%',
     },
+    containerPlageData: {
+      height: '60%',
+      width: '80%',
+      backgroundColor: '#F8F8F8',
+      borderRadius: '15',
+    }
 });
